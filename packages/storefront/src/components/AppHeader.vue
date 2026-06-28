c7c0f01fc8fb138ebc32078823162aeb0a202608
NlightN22 <oncharterliz@gmail.com>
2026-06-28 12:49:14 +0300

    fix(storefront): use same nav-btn style for Sign in button in public header

--- packages/storefront/src/components/AppHeader.vue
+++ packages/storefront/src/components/AppHeader.vue
@@ -111,8 +111,11 @@ onBeforeUnmount(() => document.removeEventListener('keydown', onKeydown));
                     </template>
 
                     <template v-else>
-                        <RouterLink to="/login" class="app-header__nav-link">
-                            <span class="app-header__nav-icon">&#128100;</span>
+                        <RouterLink to="/login" class="app-header__nav-btn">
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <circle cx="12" cy="8" r="4"/>
+                                <path d="M4 20c0-4 3.6-7 8-7s8 3 8 7"/>
+                            </svg>
                             <span>Sign in</span>
                         </RouterLink>
                     </template>
